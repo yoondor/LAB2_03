f2579ddac97b30579b09286a987ba996144bd602
5yoondori <5yoondori@snu.ac.kr>
2024-03-22 11:28:34 +0900

    2020-14489 Oh Yoon Sang

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    cout<<"Hello World!"<<endl;
-    cout<<"My Name is ~~"<<endl;
+    cout << "Hello World!" << endl;
+    cout << "My Name is 2020-14489, Oh Yoon Sang" << endl;
     return 0;
-
 }
